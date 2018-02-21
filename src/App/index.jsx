2efda58a7b2a3e438112c9a957e6e24a4541fbee
ybbarng <ybbarng@gmail.com>
2018-02-21 12:08:30 +0900

    전체화면에 dropzone을 이용해서 이미지를 업로드할 수 있게 함
    https://react-dropzone.js.org/#!/Full Screen Dropzone
    위 링크를 참고해서 구현

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
 import './style.css';
 import Map from '../Map'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      images: [],
+      dropzoneActive: false
+    }
+  }
+
+  onDragEnter = () => {
+    this.setState({
+      dropzoneActive: true
+    });
+  };
+
+  onDragLeave = () => {
+    this.setState({
+      dropzoneActive: false
+    });
+  };
+
+  onDrop = (images) => {
+    this.setState({
+      images,
+      dropzoneActive: false
+    });
+  };
+
   render() {
+    const { files, dropzoneActive } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Tripic</h1>
-        </header>
-        <Map />
-      </div>
+      <Dropzone
+        disableClick
+        style={{ position: "relative" }}
+        accept="image/jpeg, image/png"
+        onDrop={ this.onDrop }
+        onDragEnter={ this.onDragEnter }
+        onDragLeave={ this.onDragLeave }
+        >
+        { dropzoneActive && <div className="overlay">여기에 업로드할 사진을 놓으세요.</div> }
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Tripic</h1>
+          </header>
+          <Map />
+        </div>
+      </Dropzone>
     );
   }
 }
