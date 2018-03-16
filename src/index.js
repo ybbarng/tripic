0eaa37ebd8e0dbe2b757ca26786d9e3300df19f4
ybbarng <ybbarng@gmail.com>
2018-03-16 23:32:12 +0900

    어드민 페이지를 위한 라우팅 구현

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import Admin from './components/Admin';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/map" component={App} />
+        <Route path="/admin" component={Admin} />
+      </div>
+    </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
