47d53322359d68548c73475e6257c5c6f4b6dadc
ybbarng <ybbarng@gmail.com>
2018-04-30 15:06:17 +0900

    선택한 국가만 지도에 남도록 함

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -3,7 +3,25 @@ import { ComposableMap, ZoomableGlobe, Geographies, Geography } from 'react-simp
 import './style.css';
 
 class Map extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selected: null
+    }
+  }
+
+  getRegionId = (region) => (region.properties.NAME_LONG);
+
+  onClick = (region) => {
+    console.log(region);
+    this.setState({
+      selected: this.getRegionId(region)
+    });
+  }
+
   render() {
+    const { selected } = this.state;
+
     return (
       <div className="Map">
         <ComposableMap
@@ -22,33 +40,39 @@ class Map extends Component {
             center={[137, 0]}
             disablePanning
             >
-            <Geographies geography="/topojson/world.json">
-              {(geographies, projection) => geographies.map((geography, i) => (
-                <Geography
-                  key={i}
-                  geography={geography}
-                  projection={projection}
-                  style={{
-                    default: {
-                      fill: "#ECEFF1",
-                      stroke: "#607D8B",
-                      strokeWidth: 0.75,
-                      outline: "none",
-                    },
-                    hover: {
-                      fill: "#607D8B",
-                      stroke: "#607D8B",
-                      strokeWidth: 0.75,
-                      outline: "none",
-                    },
-                    pressed: {
-                      fill: "#FF5722",
-                      stroke: "#607D8B",
-                      strokeWidth: 0.75,
-                      outline: "none",
-                    },
-                  }}
-                />
+            <Geographies
+              geography="/topojson/world.json"
+              disableOptimization
+            >
+              {(geographies, projection) => geographies
+                  .filter((geography) => (selected == null || selected === this.getRegionId(geography)))
+                  .map((geography, i) => (
+                    <Geography
+                      key={this.getRegionId(geography)}
+                      geography={geography}
+                      projection={projection}
+                      onClick={this.onClick}
+                      style={{
+                        default: {
+                          fill: "#ECEFF1",
+                          stroke: "#607D8B",
+                          strokeWidth: 0.75,
+                          outline: "none",
+                        },
+                        hover: {
+                          fill: "#607D8B",
+                          stroke: "#607D8B",
+                          strokeWidth: 0.75,
+                          outline: "none",
+                        },
+                        pressed: {
+                          fill: "#FF5722",
+                          stroke: "#607D8B",
+                          strokeWidth: 0.75,
+                          outline: "none",
+                        },
+                      }}
+                    />
               ))}
             </Geographies>
           </ZoomableGlobe>
